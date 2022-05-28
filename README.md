2d29e014e787b5185b5a95f804d20fe7c56a6920
sangpyokim <rlatkdvy12@naver.com>
2022-05-28 19:28:28 +0900

    image upload

--- README.md
+++ README.md
@@ -1,46 +1,18 @@
-# Getting Started with Create React App
+# 이미지 주소 얻기 / 용량 줄이기
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 이미지 주소 얻기
 
-## Available Scripts
+- [x] : 이미지 미리보기
 
-In the project directory, you can run:
+- [x] : firebase를 사용하여 이미지 주소 얻음
 
-### `npm start`
+- [ ] : 여러개의 이미지도 가능
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+base64 // blob
+미리보기할 base64 데이터와 url을 얻을 blob 데이터을 구분해서 setstate함.
+결과에 이미지 주소랑 다운로드 버튼
+모든결과 다운로드 버튼
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+## 이미지 용량 줄이기
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+- [ ] : 고민 중
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytes, uploadBytesResumable, uploadString } from 'firebase/storage';
+import { app, storage } from './firebase'
 
 function App() {
+  const [ file, setFile ] = useState<any>();
+  const [ loading, setLoading ] = useState(false);
+  const [ download, setDownload ] = useState('')
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.target.files?.length === 0) return;
+
+    const files = e.target.files!
+    const temp: any = [];
+    for (let i = 0; i < files?.length; i++) {
+      // const fR = new FileReader()
+      // fR.readAsDataURL(files[i]);
+      // fR.onload = () => {
+      //   // temp.push(fR.result)
+      // }
+      // temp.push(files[0])
+      setFile(files[i])
+    }
+    
+  }
+
+  const onSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const storageRef = ref(storage, 'images');
+
+    
+    
+    // await uploadString(storageRef, file, 'data_url').then(snapshot => {
+    //   console.log("done")
+    // }).then(() => setLoading(false)).then()
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      } 
+    }, e => {
+      console.log(e)
+    }, () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setDownload(downloadURL)
+        console.log('File available at', downloadURL);
+      });
+    })
+
+    console.log(uploadTask)
+  }
+  
+  useEffect(() => {
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={e => onSubmit(e)} >
+        <input type={'file'} multiple={true}  accept='image/*' onChange={e => onChange(e)} />
+        <button type='submit' disabled={file && !loading ? false : true} >제출</button>
+      </form>
+
+        미리보기
+      <div>
+        <img width={700} src={file} />
+      </div>
+    결과
+      <div>
+      {download}
+      </div>
     </div>
   );
 }
